c57fd541fecf25d50e3721b0e6a75b340d5b83ee
voyccom <john@hagstrand.com>
2019-09-13 07:02:07 -0700

    fix build for a2

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@
 python compilejs.py $1 >min.js
 
 # compress css file with yui
-cat minimal/normaleyes.css icon/icon.css minimal/minimal.css keyboard.css >min.css
-java -jar ~/bin/yuicompressor/yuicompressor-2.4.2.jar min.css -o min.css --charset utf-8
+cat minimal/normaleyes.css icon/icon.css minimal/minimal.css keyboard.css |
+  sed 's/+/%2b/g'  >min.css
+wget --post-data="input=`cat min.css`" --output-document=min.css https://cssminifier.com/raw
 
 # prepare index.php for production use
 cp index.html index.php
