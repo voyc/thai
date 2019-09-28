a9f76188c3ce2ebf84b7426b710d48fd9b9c80ab
voyccom <john@hagstrand.com>
2019-09-28 06:43:50 -0700

    fix createdb sql

--- lesson/AUA/sql/createdb.sql
+++ lesson/AUA/sql/createdb.sql
@@ -1,5 +1,5 @@
 
-# create schema in database voyccom_thai as owner voyccom_jhagstrand
+/* create schema in database voyccom_thai as owner voyccom_jhagstrand */
 
 create schema thai;
 
@@ -26,8 +26,4 @@ create index on thai.vocab(thai,defnum);
 
 grant all on schema thai to voyccom_webuser64;
 grant all on table thai.dictionary to voyccom_webuser64;
-grant all on index thai_dict_thai to voyccom_webuser64;
-grant all on index thai_dict_english to voyccom_webuser64;
 grant all on table thai.vocab to voyccom_webuser64;
-grant all on index thai_vocab to voyccom_webuser64;
-
